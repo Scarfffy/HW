dc929add438864ccf28c0a7365ce0451c616ef4f
Scarfy <pppau2370@gmail.com>
2023-06-04 19:14:10 +0300

    fixed bugs and issues

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -11,21 +11,24 @@ class Program
     public static int round = 1;
     public static bool won = false;
     static string[] suits = { "Spades", "Clubs", "Hearts", "Diamonds" };
-    static string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+    //static string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
     static Random random = new Random();
 
     static void Main(string[] args)
     {
         Console.WriteLine("\nWELCOME TO THE GAME 21\n");
+        Thread.Sleep(1000);
         Console.WriteLine("What is your name?");
         P1 = Console.ReadLine();
+        Thread.Sleep(500);
         StartGame();
     }
 
     public static void StartGame()
     {
         Console.WriteLine($"\nWelcome {P1}! Let's play!");
-        Console.WriteLine("Objective: Reach the highest score without going over 21\n");
+        Console.WriteLine("Objective: Reach the highest score without going over 21, Jack - 2 points, Queen - 3 points, King - 4 points, Ace - 11 points\n");
+        Thread.Sleep(1000);
 
         while (true)
         {
@@ -34,11 +37,53 @@ class Program
                 int p1Card = GetRandomCard();
                 int cpuCard = GetRandomCard();
 
-                p1Score += p1Card;
-                cpuScore += cpuCard;
+                if (p1Card == 9) //player cards
+                {
+                    p1Score += 2;
+                }
+                else if (p1Card == 10)
+                {
+                    p1Score += 3;
+                }
+                else if (p1Card == 11)
+                {
+                    p1Score += 4;
+                }
+                else if (p1Card == 12)
+                {
+
+                    p1Score += 11;
+                }
+                else
+                {
+                    p1Score += p1Card;
+                }
+
+                if (cpuCard == 9) //bot cards
+                {
+                    cpuScore += 2;
+                }
+                else if (cpuCard == 10)
+                {
+                    cpuScore += 3;
+                }
+                else if (cpuCard == 11)
+                {
+                    cpuScore += 4;
+                }
+                else if (cpuCard == 12)
+                {
+                    cpuScore += 11;
+                }
+                else
+                {
+                    cpuScore += cpuCard;
+                }
 
                 Console.WriteLine($"{P1}, you are dealt a {GetCardString(p1Card)}");
+                Thread.Sleep(1000);
                 Console.WriteLine($"{cpu} is dealt a {GetCardString(cpuCard)}");
+                Thread.Sleep(1000);
 
                 round++;
             }
@@ -58,9 +103,31 @@ class Program
                 if (message.ToLower() == "y" && !won)
                 {
                     int p1Card = GetRandomCard();
-                    p1Score += p1Card;
+
+                    if (p1Card == 9)
+                    {
+                        p1Score += 2;
+                    }
+                    else if (p1Card == 10)
+                    {
+                        p1Score += 3;
+                    }
+                    else if (p1Card == 11)
+                    {
+                        p1Score += 4;
+                    }
+                    else if (p1Card == 12)
+                    {
+
+                        p1Score += 11;
+                    }
+                    else
+                    {
+                        p1Score += p1Card;
+                    }
 
                     Console.WriteLine($"{P1}, you are dealt a {GetCardString(p1Card)} Total value: {p1Score}");
+                    Thread.Sleep(1000);
 
                     CheckBust(p1Score, cpuScore);
 
@@ -74,11 +141,32 @@ class Program
                     while (cpuScore <= p1Score && p1Score > 0)
                     {
                         int cpuCard = GetRandomCard();
-                        cpuScore += cpuCard;
+
+                        if (cpuCard == 9)
+                        {
+                            cpuScore += 2;
+                        }
+                        else if (cpuCard == 10)
+                        {
+                            cpuScore += 3;
+                        }
+                        else if (cpuCard == 11)
+                        {
+                            cpuScore += 4;
+                        }
+                        else if (cpuCard == 12)
+                        {
+                            cpuScore += 11;
+                        }
+                        else
+                        {
+                            cpuScore += cpuCard;
+                        }
 
                         Console.WriteLine($"{cpu} is dealt a {GetCardString(cpuCard)} Total value: {cpuScore}");
 
                         CheckBust(p1Score, cpuScore);
+                        Thread.Sleep(1000);
 
                         if (cpuScore > p1Score)
                         {
@@ -143,52 +231,45 @@ class Program
     static int GetRandomCard()
     {
         int randomCards = random.Next(0, 12);
-        if (randomCards == 9)
+        switch (randomCards)
         {
-            return 2;
+            case 0:
+                return 2;
+            case 1:
+                return 3;
+            case 2:
+                return 4;
+            case 3:
+                return 5;
+            case 4:
+                return 6;
+            case 5:
+                return 7;
+            case 6:
+                return 8;
+            case 7:
+                return 9;
+            case 8:
+                return 10;
         }
-        else if (randomCards == 10)
-        {
-            return 3;
-        }
-        else if (randomCards == 11)
-        {
-            return 4;
-        }
-        else if (randomCards == 12)
-        {
-            return 11;
-        }
-        else
-        {
-            return randomCards;
-        }
+        return randomCards;
     }
 
     static string GetCardString(int cardValue)
     {
-        string rank = ranks[cardValue - 2];
         string suit = suits[random.Next(0, suits.Length)];
-
-        int score = GetCardScore(rank);
-
-        return $"{rank} of {suit} ({score} points)";
-    }
-
-    static int GetCardScore(string rank)
-    {
-        switch (rank)
+        int rank = cardValue;
+        switch (cardValue)
         {
-            case "Ace":
-                return 11;
-            case "King":
-                return 4;
-            case "Queen":
-                return 3;
-            case "Jack":
-                return 2;
-            default:
-                return int.Parse(rank);
+            case 9:
+                return $"Jack of {suit}" ;
+            case 10:
+                return $"Queen of {suit}";
+            case 11:
+                return $"King of {suit}";
+            case 12:
+                return $"Ace of {suit}";
         }
+        return $"{rank} of {suit}";
     }
 }
