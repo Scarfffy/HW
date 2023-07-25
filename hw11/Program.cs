2d8af6c325016a94b8ff7383b2288cab539e92fa
Scarfy <pppau2370@gmail.com>
2023-07-25 16:57:36 +0300

    add hw11

---
+++ hw11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CityGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: CityGenerator <output_file_path> <number_of_lines>");
+                return;
+            }
+
+            string namesFilePath = "C:\\Users\\Scarfy\\source\\repos\\hw2\\hw11\\city.txt";
+            string outputFilePath = args[0];
+            int numberOfLines = int.Parse(args[1]);
+
+            string[] names = File.ReadAllLines(namesFilePath);
+
+            Random random = new Random();
+            using (StreamWriter file = new StreamWriter(outputFilePath))
+            {
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    string city = names[random.Next(names.Length)];
+                    int area = random.Next(100, 1000);
+                    int population = random.Next(1000, 1000000);
+                    string country = names[random.Next(names.Length)];
+                    string line = $"{city}:{area};{population};{country}";
+
+                    file.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine($"Generated {numberOfLines} lines and saved to {outputFilePath}");
+        }
+    }
+}
---
+++ hw11/parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace CityParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: CityParser <input_file_path> <output_file_path>");
+                return;
+            }
+
+            string inputFilePath = args[0];
+            string outputFilePath = args[1];
+
+            List<CityInfo> cities = new List<CityInfo>();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(':');
+                if (parts.Length == 2)
+                {
+                    string[] info = parts[1].Split(';');
+                    if (info.Length == 3 && int.TryParse(info[0], out int area) && int.TryParse(info[1], out int population))
+                    {
+                        cities.Add(new CityInfo
+                        {
+                            City = parts[0],
+                            Area = area,
+                            Population = population,
+                            Country = info[2]
+                        });
+                    }
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(cities, Formatting.Indented);
+            File.WriteAllText(outputFilePath, json);
+
+            Console.WriteLine($"Parsed {cities.Count} lines and saved to {outputFilePath}");
+        }
+    }
+
+    class CityInfo
+    {
+        public string City { get; set; }
+        public int Area { get; set; }
+        public int Population { get; set; }
+        public string Country { get; set; }
+    }
+}
+
