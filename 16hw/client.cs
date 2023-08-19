6d7e4b6032ee1e48034116d78f661352ce43b970
Scarfy <pppau2370@gmail.com>
2023-08-19 23:27:17 +0300

    add hw 16

---
+++ 16hw/client.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    private static TcpClient client;
+    private static NetworkStream stream;
+    private static string username;
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            client = new TcpClient("127.0.0.1", 8888);
+            stream = client.GetStream();
+
+            // ... інший код ...
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            stream?.Close();
+            client?.Close();
+        }
+
+        Console.Write("Enter your username: ");
+        username = Console.ReadLine();
+
+        try
+        {
+            client = new TcpClient("127.0.0.1", 8888);
+            stream = client.GetStream();
+
+            Thread receiveThread = new Thread(ReceiveMessages);
+            receiveThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "exit")
+                    break;
+
+                if (message.StartsWith("@"))
+                {
+                    SendPrivateMessage(message);
+                }
+                else
+                {
+                    SendMessage(message);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            stream?.Close();
+            client?.Close();
+        }
+    }
+
+    static void ReceiveMessages()
+    {
+        byte[] data = new byte[1024];
+        while (true)
+        {
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+            Console.WriteLine(message);
+        }
+    }
+
+    static void SendMessage(string message)
+    {
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+    }
+
+    static void SendPrivateMessage(string message)
+    {
+        Console.Write("Enter recipient's username: ");
+        string recipient = Console.ReadLine();
+        string formattedMessage = $"@{recipient} {message}";
+        SendMessage(formattedMessage);
+    }
+}
---
+++ 16hw/server.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    internal static readonly Dictionary<string, ClientHandler> clients = new Dictionary<string, ClientHandler>();
+    internal static readonly object clientsLock = new object();
+
+    static void Main(string[] args)
+    {
+        TcpListener server = null;
+
+        try
+        {
+            int port = 8888;
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            Console.WriteLine("Server started on port " + port);
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                ClientHandler clientHandler = new ClientHandler(client);
+                Thread clientThread = new Thread(clientHandler.HandleClient);
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+
+    internal static void AddClient(string username, ClientHandler clientHandler)
+    {
+        lock (clientsLock)
+        {
+            clients.Add(username, clientHandler);
+        }
+    }
+
+    public static void RemoveClient(string username)
+    {
+        lock (clientsLock)
+        {
+            clients.Remove(username);
+        }
+    }
+
+    public static void BroadcastMessage(string message, ClientHandler sender = null)
+    {
+        lock (clientsLock)
+        {
+            foreach (var client in clients.Values)
+            {
+                if (client != sender)
+                {
+                    client.SendMessage(message);
+                }
+            }
+        }
+    }
+}
+
+class ClientHandler
+{
+    private TcpClient client;
+    private NetworkStream stream;
+    private string username;
+
+    public ClientHandler(TcpClient client)
+    {
+        this.client = client;
+    }
+
+    public void HandleClient()
+    {
+        stream = client.GetStream();
+
+        while (true)
+        {
+            byte[] data = new byte[1024];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string message = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+            if (message.StartsWith("@"))
+            {
+                string[] parts = message.Split(' ', 2);
+                if (parts.Length >= 2)
+                {
+                    string recipient = parts[0].Substring(1);
+                    string privateMessage = parts[1];
+                    SendPrivateMessage(privateMessage, recipient);
+                }
+                else
+                {
+                    string errorMessage = "Invalid private message format. Usage: @recipient message";
+                    SendMessage(errorMessage);
+                }
+            }
+            else
+            {
+                Server.BroadcastMessage(username + ": " + message, this);
+            }
+        }
+    }
+
+    private void SendPrivateMessage(string message, string recipient)
+    {
+        lock (Server.clientsLock)
+        {
+            if (Server.clients.TryGetValue(recipient, out var targetClient))
+            {
+                string formattedMessage = $"[Private] {username}: {message}";
+                targetClient.SendMessage(formattedMessage);
+            }
+            else
+            {
+                string errorMessage = $"User '{recipient}' not found or offline.";
+                SendMessage(errorMessage);
+            }
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+    }
+}
